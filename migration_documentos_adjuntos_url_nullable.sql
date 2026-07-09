69620baccb9ce34b240331b2098dc36645fa0467
monserratt scarletthe aravena mattamala <146583280+monsearaMatt@users.noreply.github.com>
2026-07-09 01:26:00 -0400

    subanme esa migracion pli

---
+++ migration_documentos_adjuntos_url_nullable.sql
@@ -0,0 +1,10 @@
+BEGIN;
+
+-- La columna "url" quedo como NOT NULL de un diseno viejo (antes de que los
+-- archivos se guardaran cifrados via object_key + storage_provider). El
+-- codigo actual (DocumentosAdjuntosService.upload) manda url: null a
+-- proposito, por lo que cualquier subida de archivo fallaba con 500
+-- ("null value in column url violates not-null constraint").
+ALTER TABLE documentos_adjuntos ALTER COLUMN url DROP NOT NULL;
+
+COMMIT;
